b011740b53cac44384629e182c5fcd38b169829d
Roughtron <roughtron@yandex.ru>
2022-04-25 21:23:35 +0300

    Calc task

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,39 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
+
+const OPERATIONS = {
+  SUM: 'sum',
+  SUBSTRACT: 'subtract',
+  MULTIPLY: 'multiply',
+  DIVIDE: 'divide',
+};
 
 // Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      operator: '',
+      firstOperand: 0,
+      secondOperand: 0,
+    };
+  },
+  computed: {
+    result() {
+      switch (this.operator) {
+        case OPERATIONS.SUM:
+          return this.firstOperand + this.secondOperand;
+
+        case OPERATIONS.SUBSTRACT:
+          return this.firstOperand - this.secondOperand;
+
+        case OPERATIONS.MULTIPLY:
+          return this.firstOperand * this.secondOperand;
+
+        case OPERATIONS.DIVIDE:
+          return this.firstOperand / this.secondOperand;
+
+        default:
+          return 0;
+      }
+    },
+  },
+}).mount('#app');
