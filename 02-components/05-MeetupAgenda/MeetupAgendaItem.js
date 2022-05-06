a09c968035fc597edcb53ec0e526e918d62b6ab3
Roughtron <roughtron@yandex.ru>
2022-05-06 17:59:29 +0300

    Meetup agenda

--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -1,23 +1,66 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles, agendaTypes } from './meetupService.js';
 
 export default defineComponent({
   name: 'MeetupAgendaItem',
 
+  props: {
+    agendaItem: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+
+  computed: {
+    type() {
+      return this.agendaItem.type;
+    },
+    icon() {
+      return `icon-${agendaItemIcons[this.type]}`;
+    },
+    iconSrc() {
+      return `/assets/icons/${this.icon}.svg`;
+    },
+    time() {
+      const { startsAt, endsAt } = this.agendaItem;
+
+      return `${startsAt} - ${endsAt}`;
+    },
+    computedTitle() {
+      const { title } = this.agendaItem;
+
+      if (title) return title;
+
+      return agendaItemDefaultTitles[this.type] ?? '';
+    },
+    isTalk() {
+      return this.type === agendaTypes.TALK;
+    },
+  },
+
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
+        <img
+          :src="iconSrc"
+          :alt="type"
+          class="icon"
+        />
       </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
+      <div class="agenda-item__col">{{ time }}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
-        <p class="agenda-item__talk">
-          <span>Talk Speaker</span>
+        <h3 class="agenda-item__title">
+          {{ computedTitle }}
+        </h3>
+        <p
+          v-if="isTalk"
+          class="agenda-item__talk"
+        >
+          <span>{{ agendaItem.speaker }}</span>
           <span class="agenda-item__dot"></span>
-          <span class="agenda-item__lang">EN</span>
+          <span class="agenda-item__lang">{{ agendaItem.language }}</span>
         </p>
-        <p>Description</p>
+        <p>{{ agendaItem.description }}</p>
       </div>
     </div>`,
 });
--- 02-components/05-MeetupAgenda/meetupService.js
+++ 02-components/05-MeetupAgenda/meetupService.js
@@ -1,15 +1,26 @@
+export const agendaTypes = {
+  REGISTRATION: 'registration',
+  OPENING: 'opening',
+  BREAK: 'break',
+  COFFEE: 'coffee',
+  CLOSING: 'closing',
+  AFTERPARTY: 'afterparty',
+  TALK: 'talk',
+  OTHER: 'other',
+};
+
 /**
  * Словарь заголовков по умолчанию для всех типов пунктов программы
  */
 export const agendaItemDefaultTitles = {
-  registration: 'Регистрация',
-  opening: 'Открытие',
-  break: 'Перерыв',
-  coffee: 'Coffee Break',
-  closing: 'Закрытие',
-  afterparty: 'Afterparty',
-  talk: 'Доклад',
-  other: 'Другое',
+  [agendaTypes.REGISTRATION]: 'Регистрация',
+  [agendaTypes.OPENING]: 'Открытие',
+  [agendaTypes.BREAK]: 'Перерыв',
+  [agendaTypes.COFFEE]: 'Coffee Break',
+  [agendaTypes.CLOSING]: 'Закрытие',
+  [agendaTypes.AFTERPARTY]: 'Afterparty',
+  [agendaTypes.TALK]: 'Доклад',
+  [agendaTypes.OTHER]: 'Другое',
 };
 
 /**
@@ -17,12 +28,12 @@ export const agendaItemDefaultTitles = {
  * Соответствует имени иконок в директории /assets/icons
  */
 export const agendaItemIcons = {
-  registration: 'key',
-  opening: 'cal-sm',
-  talk: 'tv',
-  break: 'clock',
-  coffee: 'coffee',
-  closing: 'key',
-  afterparty: 'cal-sm',
-  other: 'cal-sm',
+  [agendaTypes.REGISTRATION]: 'key',
+  [agendaTypes.OPENING]: 'cal-sm',
+  [agendaTypes.TALK]: 'tv',
+  [agendaTypes.BREAK]: 'clock',
+  [agendaTypes.COFFEE]: 'coffee',
+  [agendaTypes.CLOSING]: 'key',
+  [agendaTypes.AFTERPARTY]: 'cal-sm',
+  [agendaTypes.OTHER]: 'cal-sm',
 };
