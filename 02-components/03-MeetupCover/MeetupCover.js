84c06b52d70a242126f871160166671190c0291e
Roughtron <roughtron@yandex.ru>
2022-05-06 16:34:49 +0300

    Meetup cover

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -3,8 +3,19 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupCover',
 
+  props: {
+    title: String,
+    image: String,
+  },
+
+  computed: {
+    computedStyles() {
+      return [`--bg-url: url('${this.image}')`];
+    },
+  },
+
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
+    <div class="meetup-cover" :style="computedStyles">
         <h1 class="meetup-cover__title">Title</h1>
     </div>`,
 });
