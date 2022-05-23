195435a8fcc078dc0cae4ac766fa82efc7fbfa8d
Roughtron <roughtron@yandex.ru>
2022-05-23 21:06:13 +0300

    UiInputDate

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,13 +1,53 @@
 <template>
-  <ui-input />
+  <ui-input ref="inputHolder" v-model="selfModel" :type="type">
+    <template v-for="(_, name) in $slots" #[name]>
+      <slot :name="name" />
+    </template>
+  </ui-input>
 </template>
 
 <script>
 import UiInput from './UiInput';
 
+const types = {
+  DATE: 'date',
+  TIME: 'time',
+  DATETIME_LOCAL: 'datetime-local',
+};
+
+const offsets = {
+  [types.DATE]: [0, 10],
+  [types.TIME]: [11, 16],
+  [types.DATETIME_LOCAL]: [0, 16],
+};
+
 export default {
   name: 'UiInputDate',
-
   components: { UiInput },
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+      validator: (v) => Object.values(types).includes(v),
+    },
+    modelValue: {
+      type: Number,
+      default: null,
+    },
+  },
+  emits: ['update:modelValue'],
+  computed: {
+    selfModel: {
+      get() {
+        if (!this.modelValue) return '';
+
+        return new Date(this.modelValue).toISOString().substring(...offsets[this.type]);
+      },
+
+      set(value) {
+        this.$emit('update:modelValue', value ? this.$refs.inputHolder.$refs.input.valueAsNumber : '');
+      },
+    },
+  },
 };
 </script>
