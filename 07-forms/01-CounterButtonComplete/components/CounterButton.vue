9896cecce14a1c8e575480406321f39cc856014b
Roughtron <roughtron@yandex.ru>
2022-05-27 12:16:16 +0300

    CounterButton

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,33 @@
 <template>
-  <button></button>
+  <button @click="onClick">{{ selfCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+  emits: ['update:count'],
+  data() {
+    return {
+      selfCount: this.count,
+    };
+  },
+  watch: {
+    count(value) {
+      this.selfCount = value;
+    },
+  },
+  methods: {
+    onClick() {
+      this.selfCount++;
+
+      this.$emit('update:count', this.selfCount);
+    },
+  },
 };
 </script>
